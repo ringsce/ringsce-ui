50d41c30f0ee345a7e2a5387c81134fe0abd6ef4
Pedro Dias Vicente <pedrolvicente@gmail.com>
2023-12-24 14:44:05 +0000

    haxe assets being added

---
+++ game/Export/android/bin/app/src/main/java/com/sample/game/MainActivity.java
@@ -0,0 +1,7 @@
+package com.sample.game;
+
+import android.os.Bundle;
+
+public class MainActivity extends org.haxe.lime.GameActivity {
+}
+
--- game/Source/Main.hx
+++ game/Source/Main.hx
@@ -8,8 +8,17 @@ import openfl.Assets;
 // Main function
 class Main extends Sprite
 {
-	public function new()
-	{
-		super();
-	}
+	public function new() {
+        super();
+        
+        var bitmap = new Bitmap(Assets.getBitmapData("assets/openfl.png"));
+        addChild(bitmap);
+		bitmap.alpha = 0;
+        bitmap.scaleX = 0;
+        bitmap.scaleY = 0;
+        bitmap.smoothing = true;
+
+        Actuate.tween(bitmap, 3, { alpha: 1 });
+        Actuate.tween(bitmap, 6, { scaleX: 1, scaleY: 1 }).ease(Elastic.easeOut);
+    }
 }
