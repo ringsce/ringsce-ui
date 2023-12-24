e1cc86ca0201a1d30d6a26842d1f674cecd13b2d
Pedro Dias Vicente <pedrolvicente@gmail.com>
2023-12-24 17:23:26 +0000

    rewriting game center for wordpress as plugin

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Home - King and Lairs</title>
+    <title>Home - Ekron RPG & CCG</title>
     <meta name="description" content="(C) 2016-2023 Kreatyve Designs">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
