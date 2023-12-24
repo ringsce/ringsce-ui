179234176296d63e0d23dfac52a16cfc45441651
Pedro Dias Vicente <pedrolvicente@gmail.com>
2023-12-24 13:25:08 +0000

    new image for ringsce Game assets

--- game/Source/Main.hx
+++ game/Source/Main.hx
@@ -1,7 +1,11 @@
 package;
 
+// imports
+import openfl.display.Bitmap;
 import openfl.display.Sprite;
+import openfl.Assets;
 
+// Main function
 class Main extends Sprite
 {
 	public function new()
